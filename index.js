1375b901dc0c8b09ba976a546a3e3709d24ccf9f
Rabbethriah <rabbethriah@gmail.com>
2026-07-17 15:30:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ export default {
         headers.delete("host");
         headers.delete("roblox-id");
         headers.delete("user-agent");
-        headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
+        headers.set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+        // headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
 
 
         const init = {
